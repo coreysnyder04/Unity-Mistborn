b02b03d0e182d19642181b3ced8ab613858fdcd3
Corey Snyder <coreysnyder@gmail.com>
2014-07-10 23:10:24 -0400

    Basic raycasting and collision detection added

--- Assets/Scripts/PlayerPhysics.cs
+++ Assets/Scripts/PlayerPhysics.cs
@@ -7,6 +7,14 @@ public class PlayerPhysics : MonoBehaviour {
 	private Vector3 s; // Size
 	private Vector3 c; // Center
 	
+	public LayerMask collisionMask;
+	Ray ray;
+	RaycastHit hit;
+	private float skin = 0.005f; // Tiny bit of space between character and ground. Helps with bugs where ray casts through the ground and doesn't trigger a hit.
+	
+	[HideInInspector]
+	public bool grounded;
+	
 	// Use this for initialization
 	void Start () {
 		collider = GetComponent<BoxCollider>();
@@ -24,9 +32,35 @@ public class PlayerPhysics : MonoBehaviour {
 	public void Move(Vector2 moveAmount){
 		
 		float deltaY = moveAmount.y;
+		float deltaX = moveAmount.x;
 		Vector2 p = transform.position; // Player Position
 		
+		float dir;
+		float x;
+		float y;
+		for(int i = 0; i<3; i++){
+			dir = Mathf.Sign(deltaY);
+			x = (p.x + c.x - s.x/2) + s.x/2 * i; // Left, center and then right most point of collider;
+			y = p.y + c.y + s.y/2 * dir; // B ottom of Collider
+			
+			ray = new Ray(new Vector2(x,y), new Vector2(0,dir));
+			
+			if(Physics.Raycast(ray, out hit, Mathf.Abs(deltaY), collisionMask)){
+				// Get distance between player and ground
+				float dst = Vector3.Distance(ray.origin, hit.point);
+				
+				// Stop player's downward movement after coming within skin width of a collider
+				if(dst > skin){
+					deltaY = dst * dir + skin;	
+				}else{
+					deltaY = 0;	
+				}
+				grounded = true;
+				break;
+			}
+		}
 		
-		transform.Translate(moveAmount);
+		Vector2 finalTransform = new Vector2(deltaX, deltaY);
+		transform.Translate(finalTransform);
 	}
 }
