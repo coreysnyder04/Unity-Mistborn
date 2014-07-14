e6e81bc0ad08900dda86da5e291b73eee558c3d0
Corey Snyder <coreysnyder@gmail.com>
2014-07-13 22:58:38 -0400

    Added lots of new animations and scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,75 +2,119 @@ using UnityEngine;
 using System.Collections;
 
 [RequireComponent(typeof(PlayerPhysics))]
-public class PlayerController : MonoBehaviour {
+public class PlayerController : Entity {
 	
+	// Player Handling
+	public float gravity = 20;
 	public float walkSpeed = 8;
 	public float runSpeed = 12;
 	public float acceleration = 30;
-	public float gravity = 20;
 	public float jumpHeight = 12;
+	public float slideDeceleration = 10;
 	
+	// System
+	private float animationSpeed;
 	private float currentSpeed;
 	private float targetSpeed;
 	private Vector2 amountToMove;
-	private float animationSpeed;
 	
+	// States
+	private bool jumping;
+	private bool sliding;
+	
+	
+	// Components
 	private PlayerPhysics playerPhysics;
 	private Animator animator;
 	
 	
-	// Use this for initialization
 	void Start () {
 		playerPhysics = GetComponent<PlayerPhysics>();
 		animator = GetComponent<Animator>();
+		
+		animator.SetLayerWeight(1,1);
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
-		if(playerPhysics.movementStop){
+		// Reset acceleration upon collision
+		if (playerPhysics.movementStopped) {
 			targetSpeed = 0;
 			currentSpeed = 0;
 		}
-
-		if(playerPhysics.grounded){
+		
+		// If player is touching the ground
+		if (playerPhysics.grounded) {
 			amountToMove.y = 0;
-			if(Input.GetButtonDown("Jump")){
-				amountToMove.y = jumpHeight;	
+			
+			// Jump logic
+			if (jumping) {
+				jumping = false;
+				animator.SetBool("Jumping",false);
+			}
+			
+			// Slide logic
+			if (sliding) {
+				if (Mathf.Abs(currentSpeed) < .25f) {
+					sliding = false;
+					animator.SetBool("Sliding",false);
+					playerPhysics.ResetCollider();
+				}
+			}
+			
+			
+			// Jump Input
+			if (Input.GetButtonDown("Jump")) {
+				amountToMove.y = jumpHeight;
+				jumping = true;
+				animator.SetBool("Jumping",true);
+			}
+			
+			// Slide Input
+			if (Input.GetButtonDown("Slide")) {
+				sliding = true;
+				animator.SetBool("Sliding",true);
+				targetSpeed = 0;
+				
+				playerPhysics.SetCollider(new Vector3(10.3f,1.5f,3), new Vector3(.35f,.75f,0));
 			}
 		}
-
-		animationSpeed = IncrementTowards(animationSpeed, Mathf.Abs(targetSpeed), acceleration);
-		animator.SetFloat("Speed", Mathf.Abs(animationSpeed));
-
-		//Input 
-		float speed = ( Input.GetButton("Run") ) ? runSpeed : walkSpeed;
-		targetSpeed = Input.GetAxisRaw("Horizontal") * speed;
-		currentSpeed = IncrementTowards(currentSpeed, targetSpeed, acceleration);
-
+		
+		// Set animator parameters
+		animationSpeed = IncrementTowards(animationSpeed,Mathf.Abs(targetSpeed),acceleration);
+		animator.SetFloat("Speed",animationSpeed);
+		
+		// Input
+		if (!sliding) {
+			float speed = (Input.GetButton("Run"))?runSpeed:walkSpeed;
+			targetSpeed = Input.GetAxisRaw("Horizontal") * speed;
+			currentSpeed = IncrementTowards(currentSpeed, targetSpeed,acceleration);
+			
+			// Face Direction
+			float moveDir = Input.GetAxisRaw("Horizontal");
+			if (moveDir !=0) {
+				transform.eulerAngles = (moveDir>0)?Vector3.up * 180:Vector3.zero;
+			}
+		}
+		else {
+			currentSpeed = IncrementTowards(currentSpeed, targetSpeed,slideDeceleration);
+		}
+		
 		// Set amount to move
 		amountToMove.x = currentSpeed;
 		amountToMove.y -= gravity * Time.deltaTime;
 		playerPhysics.Move(amountToMove * Time.deltaTime);
-
-
-		// Face right direction
-		float movementDirection = Input.GetAxisRaw("Horizontal");
-		if(movementDirection != 0){
-			transform.eulerAngles = (movementDirection > 0) ? Vector3.up * 180 : Vector3.zero;
-		}
-
+		
 	}
 	
-	// Increase n towards target by space
-	private float IncrementTowards(float n, float target, float a){
-		if(n == target){
-			return n;
-		}else{
+	// Increase n towards target by speed
+	private float IncrementTowards(float n, float target, float a) {
+		if (n == target) {
+			return n;	
+		}
+		else {
 			float dir = Mathf.Sign(target - n); // must n be increased or decreased to get closer to target
 			n += a * Time.deltaTime * dir;
-			return (dir == Mathf.Sign(target-n)) ? n : target; // if n has now passed target then return target, otherwise reutrn n
+			return (dir == Mathf.Sign(target-n))? n: target; // if n has now passed target then return target, otherwise return n
 		}
 	}
 }
-
--- Assets/Scripts/PlayerPhysics.cs
+++ Assets/Scripts/PlayerPhysics.cs
@@ -1,132 +1,135 @@
 using UnityEngine;
 using System.Collections;
 
+
+[RequireComponent (typeof(BoxCollider))]
 public class PlayerPhysics : MonoBehaviour {
 	
-	private BoxCollider collider;
-	private Vector3 s; // Size
-	private Vector3 c; // Center
-	private Vector3 originalSize;
-	private Vector3 originalCenter;
-	private float colliderScale;
-	private int collisionDivisionsX = 3;
-	private int collisionDivisionsY = 10;
-
-	
 	public LayerMask collisionMask;
-	Ray ray;
-	RaycastHit hit;
-	private float skin = 0.005f; // Tiny bit of space between character and ground. Helps with bugs where ray casts through the ground and doesn't trigger a hit.
+	
+	private BoxCollider collider;
+	private Vector3 s;
+	private Vector3 c;
+	
+	private Vector3 originalSize;
+	private Vector3 originalCentre;
+	private float colliderScale;
+	
+	private int collisionDivisionsX = 3;
+	private int collisionDivisionsY =10;
+	
+	private float skin = .005f;
 	
 	[HideInInspector]
 	public bool grounded;
 	[HideInInspector]
-	public bool movementStop;
+	public bool movementStopped;
 	
-	// Use this for initialization
-	void Start () {
+	Ray ray;
+	RaycastHit hit;
+	
+	void Start() {
 		collider = GetComponent<BoxCollider>();
 		colliderScale = transform.localScale.x;
+		
 		originalSize = collider.size;
-		originalCenter = collider.center;
-		SetCollider(originalSize, originalCenter);
+		originalCentre = collider.center;
+		SetCollider(originalSize,originalCentre);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	
-	
-	
-	public void Move(Vector2 moveAmount){
+	public void Move(Vector2 moveAmount) {
 		
 		float deltaY = moveAmount.y;
 		float deltaX = moveAmount.x;
-		Vector2 p = transform.position; // Player Position
+		Vector2 p = transform.position;
 		
-		float dir;
-		float x;
-		float y;
+		// Check collisions above and below
 		grounded = false;
 		
-		
-		// Top/Bottom Collisions
-		for(int i = 0; i<collisionDivisionsX; i++){
-
-			dir = Mathf.Sign(deltaY);
-			x = (p.x + c.x - s.x/2) + s.x/(collisionDivisionsX-1) * i; // Left, center and then right most point of collider;
-			y = p.y + c.y + s.y/2 * dir; // Bottom of Collider
+		for (int i = 0; i<collisionDivisionsX; i ++) {
+			float dir = Mathf.Sign(deltaY);
+			float x = (p.x + c.x - s.x/2) + s.x/(collisionDivisionsX-1) * i; // Left, centre and then rightmost point of collider
+			float y = p.y + c.y + s.y/2 * dir; // Bottom of collider
 			
 			ray = new Ray(new Vector2(x,y), new Vector2(0,dir));
-			Debug.DrawRay(ray.origin, ray.direction);
+			Debug.DrawRay(ray.origin,ray.direction);
 			
-			if(Physics.Raycast(ray, out hit, Mathf.Abs(deltaY) + skin, collisionMask)){
-				// Get distance between player and ground
-				float dst = Vector3.Distance(ray.origin, hit.point);
+			if (Physics.Raycast(ray,out hit,Mathf.Abs(deltaY) + skin,collisionMask)) {
+				// Get Distance between player and ground
+				float dst = Vector3.Distance (ray.origin, hit.point);
 				
-				// Stop player's downward movement after coming within skin width of a collider
-				if(dst > skin){
-					deltaY = dst * dir - (skin * dir); // Last part is to add or subtract based on moving up/down
-				}else{
-					deltaY = 0;	
+				// Stop player's downwards movement after coming within skin width of a collider
+				if (dst > skin) {
+					deltaY = dst * dir - skin * dir;
 				}
+				else {
+					deltaY = 0;
+				}
+				
 				grounded = true;
+				
 				break;
+				
 			}
 		}
 		
-		// Left & Right Collisions
-		movementStop = false;
-		for(int i = 0; i<collisionDivisionsY; i++){
-			
-			
-			dir = Mathf.Sign(deltaX);
-			x = p.x + c.x + s.x/2 * dir; // Left, center and then right most point of collider;
-			y = p.y + c.y - s.y/2 + s.y/(collisionDivisionsY-1) * i; // Bottom of Collider
+		
+		// Check collisions left and right
+		movementStopped = false;
+		for (int i = 0; i<collisionDivisionsY; i ++) {
+			float dir = Mathf.Sign(deltaX);
+			float x = p.x + c.x + s.x/2 * dir;
+			float y = p.y + c.y - s.y/2 + s.y/3 * i;
 			
 			ray = new Ray(new Vector2(x,y), new Vector2(dir,0));
-			Debug.DrawRay(ray.origin, ray.direction);
+			Debug.DrawRay(ray.origin,ray.direction);
 			
-			if(Physics.Raycast(ray, out hit, Mathf.Abs(deltaX) + skin, collisionMask)){
-				// Get distance between player and ground
-				float dst = Vector3.Distance(ray.origin, hit.point);
+			if (Physics.Raycast(ray,out hit,Mathf.Abs(deltaX) + skin,collisionMask)) {
+				// Get Distance between player and ground
+				float dst = Vector3.Distance (ray.origin, hit.point);
 				
-				// Stop player's downward movement after coming within skin width of a collider
-				if(dst > skin){
-					deltaX = dst * dir - (skin * dir); // Last part is to add or subtract based on moving up/down
-				}else{
-					deltaX = 0;	
+				// Stop player's downwards movement after coming within skin width of a collider
+				if (dst > skin) {
+					deltaX = dst * dir - skin * dir;
 				}
-				movementStop = true;
+				else {
+					deltaX = 0;
+				}
+				
+				movementStopped = true;
 				break;
+				
 			}
 		}
 		
-		if(!grounded && !movementStop){
-		// Direction the player is moving
-			Vector3 playerDirection = new Vector3(deltaX, deltaY);
-			Vector3 origin = new Vector3( (p.x + c.x + s.x/2 * Mathf.Sign (deltaX)), (p.y + c.y + s.y/2 * Mathf.Sign(deltaY)) );
-			Debug.DrawRay(origin, playerDirection.normalized);
-			ray = new Ray(origin, playerDirection.normalized);
-			if(Physics.Raycast(ray, Mathf.Sqrt(deltaX * deltaX + deltaY * deltaY), collisionMask)){
+		if (!grounded && !movementStopped) {
+			Vector3 playerDir = new Vector3(deltaX,deltaY);
+			Vector3 o = new Vector3(p.x + c.x + s.x/2 * Mathf.Sign(deltaX),p.y + c.y + s.y/2 * Mathf.Sign(deltaY));
+			ray = new Ray(o,playerDir.normalized);
+			
+			if (Physics.Raycast(ray,Mathf.Sqrt(deltaX * deltaX + deltaY * deltaY),collisionMask)) {
 				grounded = true;
 				deltaY = 0;
 			}
 		}
 		
 		
+		Vector2 finalTransform = new Vector2(deltaX,deltaY);
 		
-		
-		Vector2 finalTransform = new Vector2(deltaX, deltaY);
-		transform.Translate(finalTransform, Space.World);
+		transform.Translate(finalTransform,Space.World);
 	}
-
-	public void SetCollider(Vector3 size, Vector3 center){
+	
+	// Set collider
+	public void SetCollider(Vector3 size, Vector3 centre) {
 		collider.size = size;
-		collider.center = center;
-
+		collider.center = centre;
+		
 		s = size * colliderScale;
-		c = center * colliderScale;
+		c = centre * colliderScale;
 	}
+	
+	public void ResetCollider() {
+		SetCollider(originalSize,originalCentre);	
+	}
+	
 }
